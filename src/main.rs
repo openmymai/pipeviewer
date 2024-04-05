ed61e6a2e63935ab9d373d733ada6b9ab8ebd59e
openmymai <maicmi@me.com>
2024-04-05 16:15:30 +0700

    is_none() to is_some()

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use clap::{arg, command, ArgAction};
 use std::env;
 use std::fs::File;
-use std::io::{self, BufReader, BufWriter, ErrorKind, Result, Read, Write};
-use clap::{arg, command, ArgAction};
+use std::io::{self, BufReader, BufWriter, ErrorKind, Read, Result, Write};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
@@ -9,10 +9,9 @@ fn main() -> Result<()> {
     let matches = command!()
         .arg(arg!([infile]))
         .arg(arg!(-o --outfile <FILE> "Write output to a file instead of stdout"))
-        .arg(arg!(-s --silent)
-        .action(ArgAction::SetTrue))
+        .arg(arg!(-s - -silent).action(ArgAction::SetTrue))
         .get_matches();
-    
+
     // let infile = matches.get_one::<String>("infile");
     let outfile = matches.get_one::<String>("outfile");
     let silent = if matches.get_flag("silent") {
@@ -23,18 +22,22 @@ fn main() -> Result<()> {
 
     dbg!(outfile, silent);
 
-    let mut reader: Box<dyn Read> = if !matches.get_one::<String>("infile").is_none() {
-        Box::new(BufReader::new(File::open(matches.get_one::<String>("infile").unwrap())?))
+    let mut reader: Box<dyn Read> = if matches.get_one::<String>("infile").is_some() {
+        Box::new(BufReader::new(File::open(
+            matches.get_one::<String>("infile").unwrap(),
+        )?))
     } else {
         Box::new(io::stdin())
     };
 
-    let mut writer: Box<dyn Write> = if !matches.get_one::<String>("outfile").is_none() {
-        Box::new(BufWriter::new(File::create(matches.get_one::<String>("outfile").unwrap())?))
+    let mut writer: Box<dyn Write> = if matches.get_one::<String>("outfile").is_some() {
+        Box::new(BufWriter::new(File::create(
+            matches.get_one::<String>("outfile").unwrap(),
+        )?))
     } else {
         Box::new(io::stdout())
     };
-    
+
     let mut total_bytes = 0;
     let mut buffer = [0; CHUNK_SIZE];
     loop {
@@ -47,12 +50,13 @@ fn main() -> Result<()> {
         if !silent {
             eprint!("\r{}", total_bytes);
         }
-        if let Err(e) = writer.write_all(&buffer[..num_read]) { // [..x] -> [0..x] -> [..] all elements
+        if let Err(e) = writer.write_all(&buffer[..num_read]) {
+            // [..x] -> [0..x] -> [..] all elements
             if e.kind() == ErrorKind::BrokenPipe {
                 break;
             }
-            return Err(e)
-        } 
+            return Err(e);
+        }
         // io::stdout().write_all(&buffer[..num_read])?; // error handling short format
     }
     if !silent {
